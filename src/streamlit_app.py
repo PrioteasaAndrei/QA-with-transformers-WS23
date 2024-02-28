f208c4a608835a9e4c80b220d4cc941c90a150f7
Mara Popescu <mara.eliana.popescu@gmail.com>
2024-02-28 21:28:52 +0100

    Added source tracing.

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from utils import *
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain.schema import format_document
 import logging
 
 st.set_page_config(page_title="💬 PubMed ChatBot")
@@ -82,6 +83,54 @@ def generate_response(input_text):
     answer = rag(rewritten_input_text)
     return answer['result']
 
+def generate_response_with_sources(input_text):
+    # rag = rag_pipeline(model_id, index_name,use_openai=True,retriever_type='ensemble')
+    llm = Ollama(model=model_id)
+    
+    ## Rewrite the input text
+    rewrite_prompt = hub.pull("langchain-ai/rewrite")
+    rewrite_llm = ChatOpenAI(temperature = 0, openai_api_key = OPENAI_API_KEY)  
+    rewriter = rewrite_prompt | rewrite_llm | StrOutputParser() | _parse
+    rewritten_input_text = rewriter.invoke({"x": input_text})   
+    logging.info(f"Original input: {input_text}. Transformed input: {rewritten_input_text}")
+
+
+    ANSWER_PROMPT = ChatPromptTemplate.from_template(
+        """
+        You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Be as verbose and educational in your response as possible. 
+        Each passage has a SOURCE which is the title of the document. When answering, cite source name of the passages you are answering from below the answer, on a new line, with a prefix of "SOURCE:".
+
+
+        context: {context}
+        Question: "{question}"
+        Answer:
+        """
+    )
+
+    DOCUMENT_PROMPT = PromptTemplate.from_template(
+        """
+        ---
+        SOURCE: {title}
+        {page_content}
+        ---
+        """
+    )
+    def _combine_documents(docs, document_prompt=DOCUMENT_PROMPT, document_separator="\n\n"):
+        doc_strings = [format_document(doc, document_prompt) for doc in docs]
+        return document_separator.join(doc_strings)
+
+    retriever = elastic_vector_search.as_retriever(search_kwargs={"k":3})
+
+    _context = {
+        "context": retriever | _combine_documents,
+        "question": RunnablePassthrough(),
+    }
+
+    chain = _context | ANSWER_PROMPT | llm | StrOutputParser()
+
+    answer = chain.invoke(rewritten_input_text)
+    return answer
+
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -92,6 +141,7 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    msg = generate_response(prompt)
+    # msg = generate_response(prompt)
+    msg = generate_response_with_sources(prompt)
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
